839801a7f07a22b91231b410702ac8bcd9e9d9c1
magnetotail <magnetotail@posteo.net>
2021-12-03 22:02:11 +0100

    pretty it up a bit

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,19 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
         return IntRange(0, input[0].length -1)
-            .map { getMostCommonBit(input, it) }
+            .map { getMostCommonBit(input, it, Selector.MOST_SELECTOR) }
             .joinToString("", "", "")
             .toInt(2) *
             IntRange(0, input[0].length -1)
-            .map { getMostCommonBit(input, it) }
+            .map { getMostCommonBit(input, it, Selector.LEAST_SELECTOR) }
             .map { it.toBinaryBoolean().not().toBinaryString() }
             .joinToString("", "", "")
             .toInt(2)
     }
 
     fun part2(input: List<String>): Int {
-        val valueOne = IntRange(0, input[0].length -1).fold(input) { list, it -> filterForMostCommon(list, it)}[0]
-        val valueTwo = IntRange(0, input[0].length -1).fold(input) { list, it -> filterForLeastCommon(list, it)}[0]
+        val valueOne = IntRange(0, input[0].length -1)
+            .fold(input) { list, it -> filterForMostCommon( list, it, Selector.MOST_SELECTOR) }[0]
+        val valueTwo = IntRange(0, input[0].length -1)
+            .fold(input) { list, it -> filterForMostCommon( list, it, Selector.LEAST_SELECTOR) }[0]
         return valueOne.toInt(2) * valueTwo.toInt(2)
     }
 
@@ -23,19 +25,19 @@ fun main() {
 
 }
 
-fun filterForMostCommon(input: List<String>, position: Int): List<String> {
-    if(input.size == 1)
-        return input
-    return input.filter { it -> it[position] == getMostCommonBit(input, position).toString()[0] }
+enum class Selector(val b: (Int, List<String>) -> Boolean) {
+    MOST_SELECTOR({num , input -> num > input.size / 2 || input.size.toFloat() / num.toFloat() == 2f}),
+    LEAST_SELECTOR({ num, input -> (num <= input.size / 2 && !(input.size.toFloat() / num.toFloat() == 2f)) })
+
 }
 
-fun filterForLeastCommon(input: List<String>, position: Int): List<String> {
+fun filterForMostCommon(input: List<String>, position: Int, selector: Selector): List<String> {
     if(input.size == 1)
         return input
-    return input.filter { it -> it[position] == getLeastCommonBit(input, position).toString()[0] }
+    return input.filter { it -> it[position] == getMostCommonBit(input, position, selector).toString()[0] }
 }
 
-fun getMostCommonBit(input: List<String>, position: Int): String {
+fun getMostCommonBit(input: List<String>, position: Int, selector: Selector): String {
     val stringLength = input[0].length
     val num = input.map { it -> it.split("") }
         .flatten() // make sequential list of all numbers
@@ -45,24 +47,9 @@ fun getMostCommonBit(input: List<String>, position: Int): String {
         .flatten() //smush everything together
         .filter { it -> it == "1" }
         .size // count ones to know if more ones or zeroes
-    val result = num > input.size / 2 || input.size.toFloat() / num.toFloat() == 2f // check if even between ones and zeroes
+    val result = selector.b(num, input) // check if even between ones and zeroes
 //    println("input: $input. Least common for position $position: $result")
     return result.toBinaryString()
-}
-
-fun getLeastCommonBit(input: List<String>, position: Int): String {
-    val stringLength = input[0].length
-    val num = input.map { it -> it.split("") }
-        .flatten() // make sequential list of all numbers
-        .filter { it -> it.isNotEmpty() } // filter empty strings from start/end of line
-        .slice(IntRange(position, input.size * stringLength - 1)) // start at bit position we want to look at
-        .windowed(1, stringLength, true) // take every nth bit
-        .flatten() //smush everything together
-        .filter { it -> it == "1" }
-        .size // count ones to know if more ones or zeroes
-    val result = (num <= input.size / 2 && !(input.size.toFloat() / num.toFloat() == 2f)).toBinaryString() // check if even between ones and zeroes
-//    println("input: $input. Least common for position $position: $result")
-    return result
 }
 
 fun Boolean.toBinaryString(): String {
